f8251217fd93f75282c50c56fbf660f7680aaefe
needles495 <94116337+needles495@users.noreply.github.com>
2022-09-17 14:31:16 +0300

    Update vmware-create-vm-from-template.ps1

--- powershell/vmware-create-vm-from-template.ps1
+++ powershell/vmware-create-vm-from-template.ps1
@@ -1,19 +1,11 @@
 $templateName = 'template'
-
 $esxName = 'esx'
-
 $dsName = 'ds'
-
 $template = Get-Template -Name $templateName
-
 $ds = Get-Datastore -Name $dsName
-
 $esx = Get-VMHost -Name $esxName
 
 $vm = New-VM -Template $template -Name stressTest -VMHost $esx -Datastore $ds -DiskStorageFormat Thin |
-
 Set-VM -NumCpu 2 -MemoryGB 4 -Confirm:$false
-
 Get-HardDisk -VM $vm | Set-HardDisk -CapacityGB 100
-
 Start-VM -VM $vm -Confirm:$false
