3764c88e8c3921f3719ee5899ab90d09b9c1172b
needles495 <94116337+needles495@users.noreply.github.com>
2022-09-21 15:05:59 +0300

    Create ring_shops_cam.sh

---
+++ bash/asterisk/ring_shops_cam.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+ip=$(cat /tmp/camera_ip)
+
+if [[ $ip == 10\.3\.2\.* ]]; then
+    echo "sklad $ip" >> /tmp/sklad
+fi
+
+if [[ $ip == 10\.3\.* ]]; then
+    SHP_ID=$(echo $ip | awk -F "." '{print $3}')
+    phone1=$(echo $ip | awk -F "." '{print $1 "." $2 "." $3 ".240"}')
+    PHONENUMBER1=$(sudo rasterisk -x "sip show peers" | grep $phone1 | head -n 1 | awk -F " " '{print $1}' | awk -F "/" '{print $1}')
+    phone2=$(echo $ip | awk -F "." '{print $1 "." $2 "." $3 ".241"}')
+    PHONENUMBER2=$(sudo rasterisk -x "sip show peers" | grep $phone2 | head -n 1 | awk -F " " '{print $1}' | awk -F "/" '{print $1}')
+    sed "s/INSERT_PHONE_NUMBER/$PHONENUMBER1/g" /home/scripts/zabbix_call/camera_not_work.call > /tmp/camera_not_work_phone1.call
+    sed "s/INSERT_PHONE_NUMBER/$PHONENUMBER2/g" /home/scripts/zabbix_call/camera_not_work.call > /tmp/camera_not_work_phone2.call
+    cp /tmp/camera_not_work_phone* /var/spool/asterisk/outgoing/
+fi
+
+if [[ $ip == 10\.1\.* ]]; then
+    SHP_ID=$(echo $ip | awk -F "." '{print $3}')
+    phone1=$(echo $ip | awk -F "." '{print $1 "." $2 "." $3 ".240"}')
+    PHONENUMBER1=$(sudo rasterisk -x "sip show peers" | grep $phone1 | head -n 1 | awk -F " " '{print $1}' | awk -F "/" '{print $1}')
+    phone2=$(echo $ip | awk -F "." '{print $1 "." $2 "." $3 ".241"}')
+    PHONENUMBER2=$(sudo rasterisk -x "sip show peers" | grep $phone2 | head -n 1 | awk -F " " '{print $1}' | awk -F "/" '{print $1}')
+    sed "s/INSERT_PHONE_NUMBER/$PHONENUMBER1/g" /home/scripts/zabbix_call/camera_not_work.call > /tmp/camera_not_work_phone1.call
+    sed "s/INSERT_PHONE_NUMBER/$PHONENUMBER2/g" /home/scripts/zabbix_call/camera_not_work.call > /tmp/camera_not_work_phone2.call
+    cp /tmp/camera_not_work_phone* /var/spool/asterisk/outgoing/
+fi
+
+if [[ $ip == 192\.168\.* ]]; then
+    SHP_ID=$(echo $ip | awk -F "." '{print $3}')
+    phone1=$(echo $ip | awk -F "." '{print $1 "." $2 "." $3 ".240"}')
+    PHONENUMBER1=$(sudo rasterisk -x "sip show peers" | grep $phone1 | head -n 1 | awk -F " " '{print $1}' | awk -F "/" '{print $1}')
+    phone2=$(echo $ip | awk -F "." '{print $1 "." $2 "." $3 ".241"}')
+    PHONENUMBER2=$(sudo rasterisk -x "sip show peers" | grep $phone2 | head -n 1 | awk -F " " '{print $1}' | awk -F "/" '{print $1}')
+    sed "s/INSERT_PHONE_NUMBER/$PHONENUMBER1/g" /home/scripts/zabbix_call/camera_not_work.call > /tmp/camera_not_work_phone1.call
+    sed "s/INSERT_PHONE_NUMBER/$PHONENUMBER2/g" /home/scripts/zabbix_call/camera_not_work.call > /tmp/camera_not_work_phone2.call
+    cp /tmp/camera_not_work_phone* /var/spool/asterisk/outgoing/
+fi
+
+if [[ $ip == 10.??.9* ]]; then
+    SHP_ID=$(echo $ip | awk -F "." '{print $2}')
+    phone1=$(echo $ip | awk -F "." '{print $1 "." $2 "." $3 ".240"}')
+    PHONENUMBER1=$(sudo rasterisk -x "sip show peers" | grep $phone1 | head -n 1 | awk -F " " '{print $1}' | awk -F "/" '{print $1}')
+    phone2=$(echo $ip | awk -F "." '{print $1 "." $2 "." $3 ".241"}')
+    PHONENUMBER2=$(sudo rasterisk -x "sip show peers" | grep $phone2 | head -n 1 | awk -F " " '{print $1}' | awk -F "/" '{print $1}')
+    sed "s/INSERT_PHONE_NUMBER/$PHONENUMBER1/g" /home/scripts/zabbix_call/camera_not_work.call > /tmp/camera_not_work_phone1.call
+    sed "s/INSERT_PHONE_NUMBER/$PHONENUMBER2/g" /home/scripts/zabbix_call/camera_not_work.call > /tmp/camera_not_work_phone2.call
+    cp /tmp/camera_not_work_phone* /var/spool/asterisk/outgoing/
+fi
